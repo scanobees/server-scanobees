61c64c1999091435277f9420e2e00b88be79d4ff
N ASIFKHAN <asifkhan4092@gmail.com>
2026-01-31 15:28:08 +0530

    new2

--- index.js
+++ index.js
@@ -38,6 +38,7 @@ app.use((req, res) => {
 });
 
 const port = process.env.PORT || 5000;
-app.listen(port, () => {
+
+app.listen(port, "0.0.0.0", () => {
   console.log(`🚀 Server running on port ${port}`);
 });
